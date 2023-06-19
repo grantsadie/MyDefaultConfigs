23e624f61c7320dfa242f93e8c4403143cf28e51
Grant Sadie <grantdsadie@gmail.com>
2023-06-19 21:50:58 +1000

    More nvim config updates

--- ArchLinux/config/nvim/init.lua
+++ ArchLinux/config/nvim/init.lua
@@ -3,8 +3,11 @@ require('user.keybindings')
 require('user.plugins')
 require('user.cmp')
 require('user.lsp')
+-- require('user.telescope')
 
 
 require('user.lsp.settings.lua-ls')
 require('user.plugin_settings.nvim-tree')
 require('user.plugin_settings.bufferline')
+require('user.plugin_settings.telescope')
+require('user.plugin_settings.treesitter')
--- ArchLinux/config/nvim/lua/user/plugins.lua
+++ ArchLinux/config/nvim/lua/user/plugins.lua
@@ -53,6 +53,7 @@ return packer.startup(function(use)
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
   use "saadparwaiz1/cmp_luasnip" -- snippet completions
+  use "hrsh7th/cmp-nvim-lsp"
 
   -- snippets
   use "L3MON4D3/LuaSnip" --snippet engine
@@ -64,7 +65,20 @@ return packer.startup(function(use)
   use "williamboman/mason-lspconfig.nvim" -- simple to use language server installer
   use 'jose-elias-alvarez/null-ls.nvim' -- LSP diagnostics and code actions
 
+    -- Telescope
+  use "nvim-telescope/telescope.nvim"
+  use 'nvim-telescope/telescope-media-files.nvim'
+
   use {'akinsho/bufferline.nvim', tag = "*", requires = 'nvim-tree/nvim-web-devicons'}
+
+  -- Treesitter
+    -- Treesitter
+  use {
+    "nvim-treesitter/nvim-treesitter",
+    run = ":TSUpdate",
+  }
+  use "p00f/nvim-ts-rainbow"
+  use "nvim-treesitter/playground"
 
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
